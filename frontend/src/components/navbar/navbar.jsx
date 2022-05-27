113b5e58a5feab7ebf176c59259645e4c7a43d02
Nikhil <heyitsnik4@gmail.com>
2022-05-27 15:50:27 +0530

    added gradient and on hover indicator

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -3,27 +3,28 @@ import "./navbar.scss";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+
   return (
     <div className="navbar">
-      <div className="right">
+      <div className="left">
         <div className="heading">ThisisNKC</div>
         <img src="assets/logo.jpg" alt="logo" />
       </div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link className="ltext" to="/">Home</Link>
         </li>
         <li>
-          <Link to="/login">LogIn</Link>
+          <Link className="ltext" to="/login">LogIn</Link>
         </li>
         <li>
-          <Link to="/aboutus">About Us</Link>
+          <Link className="ltext" to="/aboutus">About Us</Link>
         </li>
         <li>
-          <Link to="/contactus">Contact Us</Link>
+          <Link className="ltext" to="/contactus">Contact Us</Link>
         </li>
       </ul>
-    </div>
+      </div>
   );
 }
 
