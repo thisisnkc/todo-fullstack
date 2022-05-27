6146c27bf2c755571ba70d0f8b948ab7a4ba8ab9
thisisnkc <nik41041@gmail.com>
2022-05-27 17:43:37 +0530

    changed overall theme

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { BiTimeFive } from "react-icons/bi";
+import { RiLockPasswordLine } from "react-icons/ri";
 import List from "./list/List";
 import Search from "./search/search";
 import "./home.scss"
@@ -9,72 +9,94 @@ import Navbar from "../navbar/navbar";
 function Home() {
   const [task, setTask] = useState("");
   const [assign, setAssign] = useState([]);
-  const [active, setActive] = useState(true);
-  const [ search , setSearch] = useState("")
-  const [filtertodo , setFiltertodo] = useState([])
+  const [search, setSearch] = useState("")
+  const [filtertodo, setFiltertodo] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     let filter = [...assign];
-    filter = filter.filter((el)=>{
+    filter = filter.filter((el) => {
       let item = el.toLowerCase();
       return item.includes(search.toLowerCase())
     })
     setFiltertodo([...filter])
-  },[assign , search])
+  }, [assign, search])
 
   function handleChange(event) {
     setTask(event.target.value);
-    if (event.target.value === "") {
-      setActive(true);
-    } else {
-      setActive(false);
-    }
   }
 
-  function handleDelete(id){
-    const NewData = assign.filter((b ,index)=>{
-     return index !== id
+  function handleDelete(id) {
+    const NewData = assign.filter((b, index) => {
+      return index !== id
     })
     console.log(NewData);
     setAssign(NewData)
   }
-  
 
-  function handleClick() {
-    task==="" ? alert("the task feild can't be empty") :
+
+  function handleClick(event) {
+    event.preventDefault();
+    task === "" ? alert("the task feild can't be empty") :
 
       setAssign((preValue) => [...preValue, task]);
-  
-      setTask("");
+
+    setTask("");
   }
 
   return (
     <div className="home">
       <Navbar />
-      <div className="main">
-        <div className="head">
-          <div className="inplus">
-          <input
-            className="text"
-            style={active ? { borderBottom : ".2rem solid red"} : { borderBottom : ".2rem solid gray"}}
-            id="t"
-            type="text"
-            value={task}
-            onChange={handleChange}
-            placeholder="Enter Something to add to the List"
-          ></input>
-          <button className="btn" onClick={handleClick}>
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
+      <div className="parent">
+        <div className="main">
+          <div className="head">
+          <form onSubmit={handleClick} className="inplus">
+          <div className="wrapper">
+            <BiTimeFive
+              className="icon"
+              style={{
+                padding: ".2rem",
+                marginTop: "1.3rem",
+                position: "absolute",
+              }}
+            />
+            <input
+              name="text"
+              className="same"
+              id="t"
+              placeholder="Title"
+              type="text"
+              value={task}
+              onChange={handleChange}
+            ></input>
           </div>
-          <Search search={search} setSearch={setSearch}/>
+          <div className="wrapper">
+            <RiLockPasswordLine
+              className="icon"
+              style={{
+                padding: ".2rem",
+                marginTop: "1.3rem",
+                position: "absolute",
+              }}
+            />
+            <input
+              name="reminder"
+              className="same"
+              placeholder="Reminder"
+              type="time"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <input type="submit" className="btn" value="Add"></input>
+        </form>
+            <Search search={search} setSearch={setSearch} />
+          </div>
+          <div className="ul">
+            {filtertodo.map((val, index) =>
+              <List value={val} id={index} key={index} handleDelete={handleDelete} />
+            )
+            }
+          </div>
         </div>
-      <div className="ul">
-       {filtertodo.map((val , index) =>
-        <List value={val} id={index} key={index} handleDelete={handleDelete}/>
-        )
-      }
-      </div>
       </div>
     </div>
   );
